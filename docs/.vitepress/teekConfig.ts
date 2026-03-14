4a4623142d15005c91a5a0071b9bd8208cf881f2
cbofjou <2046345294@qq.com>
2026-03-14 17:22:37 +0800

    chore: 增加.gitattributes文件

--- docs/.vitepress/teekConfig.ts
+++ docs/.vitepress/teekConfig.ts
@@ -12,6 +12,7 @@ export const teekConfig = defineTeekConfig({
   footerInfo: {
     theme: {
       name: `Theme By Teek@${version}`,
+      link: "https://github.com/Kele-Bingtang/vitepress-theme-teek"
     },
     copyright: {
       createYear: 2026,
@@ -33,7 +34,7 @@ export const teekConfig = defineTeekConfig({
     name: "cbofjouの博客", // Banner 标题，默认读取 vitepress 的 title 属性
     bgStyle: "fullImg", // Banner 背景风格：pure 为纯色背景，partImg 为局部图片背景，fullImg 为全屏图片背景
     pureBgColor: "#28282d", // Banner 背景色，bgStyle 为 pure 时生效
-    imgSrc: ["bannerImg/bg1.jpeg", "bannerImg/bg2.jpg", "bannerImg/bg3.jpeg", "bannerImg/bg4.jpeg", "bannerImg/bg5.jpeg", "bannerImg/bg6.jpeg", "bannerImg/bg7.jpeg", "bannerImg/bg8.jpeg"], // Banner 图片链接。bgStyle 为 partImg 或 fullImg 时生效
+    imgSrc: ["/bannerImg/bg1.jpeg", "/bannerImg/bg2.jpg", "/bannerImg/bg3.jpeg", "/bannerImg/bg4.jpeg", "/bannerImg/bg5.jpeg", "/bannerImg/bg6.jpeg", "/bannerImg/bg7.jpeg", "/bannerImg/bg8.jpeg"], // Banner 图片链接。bgStyle 为 partImg 或 fullImg 时生效
     imgInterval: 15000, // 当多张图片时（imgSrc 为数组），设置切换时间，单位：毫秒
     imgShuffle: false, // 图片是否随机切换，为 false 时按顺序切换，bgStyle 为 partImg 或 fullImg 时生效
     imgWaves: false, // 是否开启 Banner 图片波浪纹，bgStyle 为 fullImg 时生效
