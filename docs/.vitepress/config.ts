d911073d05087932027ff44a2ca53c1179b382be
cbofjou <2046345294@qq.com>
2026-03-14 20:43:39 +0800

    fix: 引入vite-plugin-image-optimizer插件，解决图片加载缓慢问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vitepress";
 import llmstxt from "vitepress-plugin-llms";
 import { teekConfig } from "./teekConfig";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 const description = [
   "欢迎来到 cbofjou 的个人博客，这里是我记录学习和生活的地方。希望我的分享能为你带来帮助和启发！"
 ].toString();
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
   extends: teekConfig,
   title: "blog",
@@ -24,24 +24,13 @@ export default defineConfig({
     ["meta", { property: "og:description", description }],
     ["meta", { name: "description", description }],
     ["meta", { name: "author", content: "Teek" }],
-    // 禁止浏览器缩放
-    // [
-    //   "meta",
-    //   {
-    //     name: "viewport",
-    //     content: "width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
-    //   },
-    // ],
     ["meta", { name: "keywords", description }],
   ],
   markdown: {
-    // 开启行号
     lineNumbers: true,
     image: {
-      // 默认禁用；设置为 true 可为所有图片启用懒加载。
       lazyLoading: true,
     },
-    // 更改容器默认值标题
     container: {
       tipLabel: "提示",
       warningLabel: "警告",
@@ -50,23 +39,7 @@ export default defineConfig({
       detailsLabel: "详情",
     },
   },
-  // sitemap: {
-  //   hostname: "", // ** 换成你的域名
-  //   transformItems: (items) => {
-  //     const permalinkItemBak: typeof items = [];
-  //     // 使用永久链接生成 sitemap
-  //     const permalinks = (globalThis as any).VITEPRESS_CONFIG.site.themeConfig
-  //       .permalinks;
-  //     items.forEach((item) => {
-  //       const permalink = permalinks?.map[item.url];
-  //       if (permalink)
-  //         permalinkItemBak.push({ url: permalink, lastmod: item.lastmod });
-  //     });
-  //     return [...items, ...permalinkItemBak];
-  //   },
-  // },
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     logo: "/logo.png",
     darkModeSwitchLabel: "主题",
     sidebarMenuLabel: "菜单",
@@ -95,10 +68,25 @@ export default defineConfig({
     }
   },
   vite: {
-    plugins: [llmstxt() as any],
+    plugins: [
+      llmstxt() as any,
+      ViteImageOptimizer({
+        test: /\.(jpe?g|png|webp)$/i,
+        png: {
+          quality: 80,
+        },
+        jpeg: {
+          quality: 80,
+        },
+        jpg: {
+          quality: 80,
+        },
+        webp: {
+          quality: 80,
+        },
+        exclude: /node_modules/,
+        includePublic: true,
+      })
+    ],
   },
-  // transformHtml: (code, id, context) => {
-  //   if (context.page !== "404.md") return code;
-  //   return code.replace("404 | ", "");
-  // },
 });
