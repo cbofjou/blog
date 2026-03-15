72145da44aafc33fb7f30e60ec2d1981c210be11
cbofjou <2046345294@qq.com>
2026-03-15 14:06:40 +0800

    feat: 新增声明组建

---
+++ docs/globalVariable/infoVariable.ts
@@ -0,0 +1,8 @@
+var Prefix = "       ";
+
+export default {
+    statement: Prefix + 
+    "本站知识来自于站主的学习笔记、项目经历、以及对技术的个人理解和总结，" +
+    "旨在分享观点，不保证知识的准确性，切勿轻易取信。           Σ( ° △ °|||)︴",
+    github: "https://github.com/cbofjou"
+}
